f7ea81a84072874f2010f83ab6c5665ff367e9a5
lavkush <lavkushsingh1998@gmail.com>
2024-08-22 17:33:38 +0530

    Footer -> Footer.jsx and some fixes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css'; // Import the CSS file for styling
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Contact: <a href="mailto:lavkushsingh1998@gmail.com">lavkushsingh1998@gmail.com</a></p>
+            <p>WhatsApp: <a href="https://wa.me/+918127173818">LAVKUSH SINGH</a></p>
+
+        </footer>
+    );
+};
+
+export default Footer;
+
+//modi
